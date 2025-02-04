dfe097e971ae02a83103d1630797a57edb9601be
unknown <betim009@gmail.com>
2025-02-04 11:06:37 -0300

    update

--- dl.py
+++ dl.py
@@ -1,6 +1,6 @@
 import yt_dlp
 
-url = "https://www.youtube.com/watch?v=OoIbuc6bJIk&ab_channel=Djonga"
+url = "https://www.youtube.com/watch?v=uJM4b-BRTWI&ab_channel=VintageCulture"
 ydl_opts = {"format": "best"}  # Isso baixa um único arquivo sem precisar do ffmpeg
 
 with yt_dlp.YoutubeDL(ydl_opts) as ydl:
